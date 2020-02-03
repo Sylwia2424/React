1f55c58182a3cf16b57666a03182d96ee35cecb5
Sylwia <sylwia_24_24@wp.pl>
2020-02-03 18:05:14 +0100

    Change for heroku

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 /* global*/
 
-const jsonServer = require('http-server dist');
-const server = jsonServer.create();
-const router = jsonServer.router('dist/index.html');
-const middlewares = jsonServer.defaults({
-  static: 'dist',
-});
-const port = process.env.PORT || 3131;
+//const jsonServer = require('http-server dist');
+//const server = jsonServer.create();
+//const router = jsonServer.router('dist/index.html');
+//const middlewares = jsonServer.defaults({
+//static: 'dist',
+//});
+//const port = process.env.PORT || 3131;
 
-server.use(middlewares);
-server.use(router);
+//server.use(middlewares);
+//server.use(router);
 
-server.listen(port);
+//server.listen(port);
